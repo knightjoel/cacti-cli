d5ce0ad74e74d42ce11de082b96e65b2431bc2b9
Joel Knight <knight.joel@gmail.com>
2007-10-27 21:54:08 +0000

    Add command line options so that contacts can be added to specific thresholds only.
    -u username
    -d underlying device's description
    -h underlying device's hostname

--- add_threshold_contact.php
+++ add_threshold_contact.php
@@ -35,38 +35,80 @@ $no_http_headers = true;
 include(dirname(__FILE__) . "/../include/global.php");
 include_once($config["base_path"] . "/lib/auth.php");
 
-/* argv[0] is the name of the script */
-array_shift($_SERVER["argv"]);
+$options = getopt("d:h:u:");
 
-foreach ($_SERVER["argv"] as $username) {
-	$sql = "SELECT ua.id, ua.username, contacts.data," .
-		"contacts.id AS contact_id " .
-		"FROM plugin_thold_contacts AS contacts " .
-		"INNER JOIN user_auth AS ua ON ua.id = contacts.user_id  " .
-		"WHERE ua.username = '$username'";
-	$user = db_fetch_row($sql);
-	if (count($user) == 0) {
-		printf("Error: User %s does not have an email address.\n", $username);
-		continue;
-	}
-	printf("Adding %s (%s) to thresholds:\n", $username, $user["data"]);
+function usage() {
+	global $_SERVER;
+	$me = $_SERVER["argv"][0];
 
-	$sql = "SELECT thold.id AS thold_id, dtd.name_cache, dtr.data_source_name " .
-		"FROM thold_data AS thold ".
-		"INNER JOIN data_template_data AS dtd ON thold.rra_id = dtd.local_data_id " .
-		"INNER JOIN data_template_rrd AS dtr ON thold.data_id = dtr.id " .
-		"ORDER BY dtd.name_cache ASC"; 
-	$thresholds = db_fetch_assoc($sql);
-	foreach ($thresholds as $th) {
-		printf("\t%s [%s]\n", $th["name_cache"], $th["data_source_name"]);
-		$sql = "DELETE FROM plugin_thold_threshold_contact " .
-			"WHERE thold_id = $th[thold_id] " .
-			"AND contact_id = $user[contact_id]";
-		db_execute($sql);
-		$sql = "INSERT INTO plugin_thold_threshold_contact " .
-			"VALUES ($th[thold_id], $user[contact_id])";
-		db_execute($sql);
-	}
+	print <<<END
+Usage: $me [-d <descr>] [-h <hostname>] -u <username>
+
+	<descr> Only add <username> as a contact if the threshold
+                belongs to a device with description <descr>.
+                Wildcards can be used by using '%'.
+        <hostname> Only add <username> as a contact if the threshold
+                   belongs to a device with hostname <hostname>.
+                   Wildcards can be used by using '%'.
+        <username> The Cacti user to add as a contact. Unless -d
+                   and/or -h are specified, the user will be added
+                   to all thresholds.
+
+END;
+	exit(1);
+}
+
+if (count($_SERVER["argv"]) < 2)
+	usage();
+
+if (gettype($options["u"]) != "string") {
+	printf("Please specify exactly one username using the -u switch.\n\n");
+	usage(1);
+}
+
+$sql = "SELECT ua.id, ua.username, contacts.data," .
+	"contacts.id AS contact_id " .
+	"FROM plugin_thold_contacts AS contacts " .
+	"INNER JOIN user_auth AS ua ON ua.id = contacts.user_id  " .
+	"WHERE ua.username = '$options[u]'";
+$user = db_fetch_row($sql);
+if (count($user) == 0) {
+	printf("Error: User %s does not have an email address.\n", $options["u"]);
+	exit(1);
+}
+printf("Adding %s (%s) to thresholds:\n", $options["u"], $user["data"]);
+
+$where = array();
+if (isset($options["d"])) {
+	array_push($where, "host.description LIKE '$options[d]'");
+}
+if (isset($options["h"])) {
+	array_push($where, "host.hostname LIKE '$options[h]'");
+}
+if (count($where)) {
+	$where_sql = sprintf("WHERE %s", join(" AND ", $where));
+} else {
+	$where_sql = "";
+}
+
+$sql = "SELECT thold.id AS thold_id, dtd.name_cache, dtr.data_source_name " .
+	"FROM thold_data AS thold ".
+	"INNER JOIN data_template_data AS dtd ON thold.rra_id = dtd.local_data_id " .
+	"INNER JOIN data_template_rrd AS dtr ON thold.data_id = dtr.id " .
+	"INNER JOIN data_local AS dl ON dtd.local_data_id = dl.id " .
+	"INNER JOIN host ON host.id = dl.host_id " .
+	$where_sql .
+	"ORDER BY dtd.name_cache ASC"; 
+$thresholds = db_fetch_assoc($sql);
+foreach ($thresholds as $th) {
+	printf("\t%s [%s]\n", $th["name_cache"], $th["data_source_name"]);
+	$sql = "DELETE FROM plugin_thold_threshold_contact " .
+		"WHERE thold_id = $th[thold_id] " .
+		"AND contact_id = $user[contact_id]";
+	db_execute($sql);
+	$sql = "INSERT INTO plugin_thold_threshold_contact " .
+		"VALUES ($th[thold_id], $user[contact_id])";
+	db_execute($sql);
 }
 
 ?>
